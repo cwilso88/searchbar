13ed887fb31e2643ce146faabc0581fec7f3c79d
cwilso88 <chasity.wilson4@gmail.com>
2021-11-03 02:21:04 -0400

    mapping through the books

---
+++ src/features/Books.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Books = ({ books }) => {
+    return books.map((book, index) => {
+        return (
+            <ul key={index}>
+                <li>Book: {book.name}</li>
+                <li>Author: {book.author}</li>
+            </ul>
+        )
+    })
+}
+
+export default Books;
