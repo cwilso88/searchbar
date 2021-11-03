eb58e5ba71236a5401e6ab1760e68c0621017fa7
cwilso88 <chasity.wilson4@gmail.com>
2021-11-03 03:59:25 -0400

    imported names from the namelist

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import React, { useState } from 'react'
 import { books } from "./bookshelf"
+import { names } from "./namelist"
 import Books from './features/Books';
 import Search from './features/Search';
+import Names from './features/name_list/Names'
+import Searchbar from './features/name_list/Searchbar'
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState("");
@@ -11,12 +14,19 @@ const App = () => {
   const filteredBooks = books.filter((book) => {
     return book.name.includes(searchTerm);
   });
+  const filteredNames = names.filter((name) => {
+    return names.name.includes(searchTerm);
+  })
 
 
   return (
     <section>
       <Search name="search" onSearch={handleInputChange}/>
       <Books books={filteredBooks}/>
+
+      <div>
+    
+      </div>
     </section>
   );
 }
