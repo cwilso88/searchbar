402ee4bbb3e4b66189668939b437a338d3daaf90
cwilso88 <chasity.wilson4@gmail.com>
2021-11-03 03:52:23 -0400

    map through names

--- src/features/name_list/Names.jsx
+++ src/features/name_list/Names.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 
 const Names = ({ names }) => {
-
+    return names.map((name, index) => {
+        return (
+            <ul>
+                <li>{name.name}</li>
+            </ul>
+        )
+    })
 }
 
 export default Names;
